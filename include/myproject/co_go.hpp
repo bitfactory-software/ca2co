3e337d9b0d4f46283a9ef037e841f764ffa1e7b9
Andreas Pfaffenbichler <andreas.pfaffenbichler@bitfactory.at>
2025-10-31 19:39:42 +0100

    co_go is nearly there

---
+++ include/myproject/co_go.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <myproject/co_go_export.hpp>
+
+[[nodiscard]] CO_GO_EXPORT int factorial(int) noexcept;
+
+[[nodiscard]] constexpr int factorial_constexpr(int input) noexcept
+{
+  if (input == 0) { return 1; }
+
+  return input * factorial_constexpr(input - 1);
+}
+
--- src/co_go/co_go.cpp
+++ src/co_go/co_go.cpp
@@ -1,4 +1,4 @@
-#include <myproject/sample_library.hpp>
+#include <co_go/co_go.hpp>
 
 int factorial(int input) noexcept
 {
